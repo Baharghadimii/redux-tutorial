73b30fa585c93580e5b7b8dfb0251159d7fd430c
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-14 12:17:31 -0800

    actions updated

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -4,9 +4,11 @@ export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload }
 }
 export function getData() {
-  return fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(response => response.json())
-    .then(json => {
-      return { type: "DATA_LOADED", payload: json };
-    });
+  return function (dispatch) {
+    return fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(json => {
+        dispatch({ type: "DATA_LOADED", payload: json });
+      });
+  };
 }
